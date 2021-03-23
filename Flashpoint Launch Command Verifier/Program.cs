eb0c6566ffbd3efb8248090a83cb03f6dc651168
Jan Štěch <honza.stech@gmail.com>
2021-03-23 09:43:51 +0100

    Initial commit

---
+++ Flashpoint Launch Command Verifier/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace Flashpoint_Launch_Command_Verifier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Flashpoint Launch Command Verifier by Shady";
+
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("Fetching the database, this should only take a few seconds...");
+            WebClient client = new WebClient();
+            Stream stream = client.OpenRead("https://bluebot.unstable.life/launch-commands");
+            StreamReader reader = new StreamReader(stream);
+            string content = reader.ReadToEnd();
+            Console.WriteLine("Successfully fetched database of launch commands");
+            Console.ResetColor();
+
+            while (true)
+            {
+                Console.Write("\n\n");
+                Console.WriteLine("Enter the launch command you want to verify:");
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                string launchConmmand = Console.ReadLine();
+
+                Console.Clear();
+                Console.WriteLine(launchConmmand);
+
+                if (content.Contains(launchConmmand))
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("This launch command is already present in the database.");
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("This launch command is unique.");
+                }
+
+                Console.ResetColor();
+            }
+        }
+    }
+}
---
+++ README.txt
@@ -0,0 +1,7 @@
+How to use this little utility
+==============================
+Well, it's simple. Just launch the .exe application (Flashpoint Launch Command Verifier.exe) and wait a few seconds for the database of launch commands to load. Then, just paste your launch command into the console by right-clicking and hit enter. The application will immediately tell you if the bot would mark this as a duplicate.
+
+Are you getting warning, that the file is a virus?
+==================================================
+Well, don't worry about that. Your antivirus most likely marks anything with a .exe extension that you downloaded from the internet and doesn't have a certificate as a virus. If you're worried, you can check out the simple source code here: https://github.com/HonzaSTECH/Flashpoint-Launch-Command-Verifier
